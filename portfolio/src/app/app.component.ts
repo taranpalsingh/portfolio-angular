a3d7070d2afd1399f548ddc825aeb83155482cdd
taranpalsingh <taranpal.singh23@gmail.com>
2022-12-21 18:58:47 -0500

    footer final

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor() {
+    // document.documentElement.style.setProperty('--primary-color', '#121212');
+    document.documentElement.style.setProperty('--secondary-background-color','#333333');
+    document.documentElement.style.setProperty('--background-color', '#2a2a2a'); 
+  }
 }
